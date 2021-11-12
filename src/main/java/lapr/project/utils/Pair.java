fe8a5595865ab435197032197cd71fa54d74a5dc
rafal <1201566@isep.ipp.pt>
2021-11-12 17:14:06 +0000

    <G76-59>  #comment[US107] Reverting commit  [Implementation]

--- src/main/java/lapr/project/utils/Pair.java
+++ src/main/java/lapr/project/utils/Pair.java
@@ -1,6 +1,7 @@
 package lapr.project.utils;
 
 
+import java.util.Objects;
 
 public class Pair<T, U> {
 
@@ -20,6 +21,13 @@ public class Pair<T, U> {
         return this.second;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+    }
 
 
 }
