97d6aa54a8413befea89f32ea973b1a8ec98b529
jorgefcferreira <1201564@isep.ipp.pt>
2021-12-03 11:15:22 +0000

    <G76-99> #time 4h  #comment US206 PL/SQL Solution

--- BaseDados/US206/US206.sql
+++ BaseDados/US206/US206.sql
@@ -1,23 +1,24 @@
 --US206
-create or replace function fnc_getContainers (idd integer, dat Date, codeFac Facility.codeFacility%TYPE)
+create or replace function fnc_getContainersToLoad (idShipCap integer, codePort String)
 RETURN SYS_REFCURSOR IS
 cursor_containers SYS_REFCURSOR;
+nextDate Date;
+
 BEGIN
- open cursor_containers for 
-select container.numberContainer,container.type,container.load, position.x, position.y,position.z, facility.namefacility, manifest.datemanifest from Container, ContainerManifest ,Position, Manifest, Facility,TripFacility,Trip, Vehicle, Ship
-WHERE Container.numberContainer = ContainerManifest.nrContainer 
+
+SELECT MIN(dateManifest) INTO nextDate FROM manifest,TripFacility, Facility, Trip where dateManifest > SYSDATE AND Manifest.typeManifest = 'load' 
+AND Manifest.idTrip = TripFacility.idTrip AND TripFacility.idTrip = Trip.idTrip AND TripFacility.codeFacility = codePort AND Trip.idShipCaptain = idShipCap;
+
+open cursor_containers for 
+select container.numberContainer,container.type,container.load, position.x, position.y,position.z, facility.nameFacility, manifest.datemanifest from Container, ContainerManifest ,Position, Manifest, TripFacility, Facility
+WHERE Container.numberContainer LIKE ContainerManifest.nrContainer 
 AND ContainerManifest.idManifest = Manifest.idManifest
 AND ContainerManifest.idPosition = Position.idPosition
-AND dat < Manifest.dateManifest 
-AND Manifest.typeManifest = 'load' 
-AND manifest.idvehicle = Vehicle.idvehicle
-AND manifest.idmanifest = facility.idmanifest
-AND codeFac = Facility.codeFacility
-AND TripFacility.codeFacility = Facility.codeFacility
-AND Trip.idTrip = TripFacility.idTrip
-AND Vehicle.idvehicle = ship.idvehicle
-AND Ship.idShipCaptain = idd;
-
+AND Manifest.typeManifest LIKE 'load'
+AND nextDate = Manifest.dateManifest
+AND Manifest.idTrip = TripFacility.idTrip
+AND TripFacility.codeFacility = codePort
+AND Facility.codeFacility = codePort;
 return cursor_containers;
 end;
 /
@@ -35,10 +36,11 @@ v_z                   INTEGER;
 v_nameFacility        Facility.nameFacility%TYPE;
 v_dateManifest        date;
 begin
-l_containers := fnc_getContainers (1,TO_DATE('2021/12/02 04:34:21', 'yyyy/mm/dd hh24:mi:ss'),3);
+l_containers := fnc_getContainersToLoad(1,3);
 loop
 fetch l_containers into v_numberContainer, v_type, v_load, v_x, v_y, v_z, v_nameFacility, v_dateManifest;
 exit when l_containers%notfound;
 dbms_output.put_line('CONTAINER NUMBER - ' || v_numberContainer ||'  TYPE - '|| v_type || '  LOAD - ' || v_load ||'  POSITION (X,Y,Z) - '|| v_x  ||','|| v_y ||','|| v_z ||'   NEXT PORT - '|| v_nameFacility || ' DATE - '||v_dateManifest);
 end loop;
 end;
+/
