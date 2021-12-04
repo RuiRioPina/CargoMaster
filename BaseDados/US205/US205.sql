dd045ba97f3494927124fb95742a4ef488af921f
jorgefcferreira <1201564@isep.ipp.pt>
2021-12-04 11:43:18 +0000

    <G76-98> #time 10m #comment US205 PL/SQL solution update

--- BaseDados/US205/US205.sql
+++ BaseDados/US205/US205.sql
@@ -9,7 +9,7 @@ SELECT MIN(dateManifest) INTO nextDate FROM manifest,TripFacility, Facility, Tri
 AND Manifest.idTrip = TripFacility.idTrip AND TripFacility.idTrip = Trip.idTrip AND TripFacility.codeFacility = codePort AND Trip.idShipCaptain = idShipCap;
 
 open cursor_containers for 
-select container.numberContainer,container.type,container.load, position.x, position.y,position.z, facility.nameFacility, manifest.datemanifest from Container, ContainerManifest ,Position, Manifest,Facility, TripFacility
+select container.numberContainer,container.type,container.load, position.x, position.y,position.z, facility.nameFacility, manifest.datemanifest from Container, ContainerManifest ,Position, Manifest,Facility, TripFacility,Trip
 WHERE Container.numberContainer LIKE ContainerManifest.nrContainer 
 AND ContainerManifest.idPosition = Position.idPosition
 AND ContainerManifest.idManifest = Manifest.idManifest
@@ -17,7 +17,9 @@ AND Manifest.dateManifest = nextDate
 AND Manifest.codeFacility = codePort
 AND Manifest.codeFacility = TripFacility.codeFacility
 AND TripFacility.idTrip = Manifest.idTrip
-AND Facility.codeFacility = TripFacility.codeFacility;
+AND Facility.codeFacility = TripFacility.codeFacility
+AND TripFacility.idTrip = Trip.idTrip
+AND Trip.idShipCaptain = idShipCap;
 return cursor_containers;
 end;
 /
@@ -41,4 +43,3 @@ exit when l_containers%notfound;
 dbms_output.put_line('CONTAINER NUMBER - ' || v_numberContainer ||'  TYPE - '|| v_type || '  LOAD - ' || v_load ||'  POSITION (X,Y,Z) - '|| v_x  ||','|| v_y ||','|| v_z ||'   NEXT PORT - '|| v_nameFacility || ' DATE - '||v_dateManifest);
 end loop;
 end;
-/
