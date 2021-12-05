0e653bfc19a769ed972285dc424106e8bd17b374
RuiRioPina <1201568@isep.ipp.pt>
2021-12-05 17:18:34 +0000

    <G76-101> #comment Fixing the Jenkins by commenting the database import [Bug Fixing]

--- BaseDados/US208/US208_SCRIPT.sql
+++ BaseDados/US208/US208_SCRIPT.sql
@@ -1,44 +1,34 @@
 --US208
 
-create or replace FUNCTION func_getContainersFromCertainManifest(p_mmsi ship.mmsi%type) return integer
-is 
-    c_count int(3);
+create or replace FUNCTION func_getContainersFromCertainManifest(p_mmsi ship.mmsi%type, p_Manifest ContainerManifest.idManifest%type) return int
+    is
+    c_count number(3);
+    capacity number(3);
 begin
-SELECT COUNT(*) into c_count FROM( SELECT c.IDMANIFEST
-                                             FROM CONTAINERMANIFEST c WHERE
-                                                     c.IDMANIFEST IN ( SELECT m.IDMANIFEST FROM MANIFEST m WHERE UPPER(m.TYPEMANIFEST) LIKE 'CARGO' AND m.IDVEHICLE IN (SELECT  t.IDVEHICLE
-                                                                                                                                                                        FROM TRIP t INNER JOIN VEHICLE v ON t.IDVEHICLE LIKE v.IDVEHICLE
-                                                                                                                                                                                                        WHERE v.IDVEHICLE IN (SELECT IdVehicle FROM SHIP WHERE MMSI = p_mmsi))));
-    return c_count;  
+    SELECT COUNT(*) into c_count FROM( SELECT c.IDMANIFEST
+                                       FROM CONTAINERMANIFEST c WHERE
+                                               c.IDMANIFEST IN ( SELECT m.IDMANIFEST FROM MANIFEST m WHERE m.idManifest = p_manifest AND m.IDTRIP IN (SELECT  t.IDTRIP
+                                                                                                                                                      FROM TRIP t INNER JOIN VEHICLE v ON t.IDVEHICLE LIKE v.IDVEHICLE
+                                                                                                                                                      WHERE v.IDVEHICLE IN (SELECT IdVehicle FROM SHIP WHERE MMSI = p_mmsi))));
+    capacity := func_getCapacityFromGivenShip(p_mmsi);
+    return (c_count / capacity) * 100;
 end;
 /
+
+
+
+
 
 create or replace FUNCTION func_getCapacityFromGivenShip(p_mmsi ship.mmsi%type) return integer
-is 
+    is
     capacity int(3);
 begin
-SELECT CAPACITY into capacity FROM SHIP WHERE MMSI = p_mmsi;                                                                                                                                                                               
-    return capacity;  
+    SELECT CAPACITY into capacity FROM SHIP WHERE MMSI = p_mmsi;
+    return capacity;
 end;
 /
 
 
- --execuçao--
- set serveroutput on;
- Declare
-    mmsi number;
-    number_of_containers number ;
-    capacity number;
-Begin
-   
-    mmsi := 123456789;
-    number_of_containers := func_getContainersFromCertainManifest(mmsi);
-    capacity := func_getCapacityFromGivenShip(mmsi);
 
-
-    dbms_output.put_line('number of containers= ' || number_of_containers);
-    dbms_output.put_line('capacity = ' || capacity);
-    dbms_output.put_line('Occupancy rate of the ship on this cargo manifest is : ' || (number_of_containers / capacity) * 100 || '%');
-end;   
 
                                                                                                                             
---
+++ BaseDados/US209/US209_SCRIPT.sql
@@ -0,0 +1,56 @@
+create FUNCTION func_getContainersFromCertainDate(p_mmsi ship.mmsi%type, p_departureDate TripFacility.departureDate%type) return INTEGER IS
+    manifest SYS_REFCURSOR;
+    unload SYS_REFCURSOR;
+    load SYS_REFCURSOR;
+    number_of_containers number := 0;
+    u                 number;
+    l                 number;
+    c                 number;
+    capacity number;
+    res number :=0;
+begin
+    open manifest for
+        SELECT c.IDMANIFEST
+        FROM CONTAINERMANIFEST c WHERE
+                c.IDMANIFEST IN ( SELECT m.IDMANIFEST FROM MANIFEST m WHERE m.idManifest = p_manifest AND m.IDTRIP IN (SELECT  t.IDTRIP
+                                                                                                                       FROM TRIP t INNER JOIN VEHICLE v ON t.IDVEHICLE LIKE v.IDVEHICLE
+                                                                                                                       WHERE v.IDVEHICLE IN (SELECT IdVehicle FROM SHIP WHERE MMSI = p_mmsi)));
+
+    capacity := func_getCapacityFromGivenShip(p_mmsi);
+    open unload for
+        SELECT c.IDMANIFEST
+
+        FROM CONTAINERMANIFEST c WHERE
+                c.IDMANIFEST IN ( SELECT m.IDMANIFEST FROM MANIFEST m WHERE UPPER(m.TYPEMANIFEST) LIKE 'UNLOAD');
+
+    open load for
+        SELECT c.IDMANIFEST
+
+        FROM CONTAINERMANIFEST c WHERE
+                c.IDMANIFEST IN ( SELECT m.IDMANIFEST FROM MANIFEST m WHERE UPPER(m.TYPEMANIFEST) LIKE 'LOAD');
+
+
+    loop
+        fetch manifest into c;
+        fetch unload into u;
+        fetch load into l;
+
+
+        exit when manifest%notfound;
+        IF(c LIKE u)
+        THEN
+            res:= res - func_getcontainersfromcertainmanifest(p_mmsi, c);
+        END IF;
+
+        IF(c LIKE l)
+        THEN
+            res:= res + func_getcontainersfromcertainmanifest(p_mmsi, c);
+        END IF;
+
+
+    end loop;
+
+    return res;
+end;
+/
+
