dc4464e8e1b96e3e988c4110905e8cbd66172d30
RuiRioPina <1201568@isep.ipp.pt>
2021-12-20 17:04:36 +0000

    <G76-168> #time 2h #comment Reviewing the implementation of the US308. [Review]

--- BaseDados/US308/US308.sql
+++ BaseDados/US308/US308.sql
@@ -1,17 +1,27 @@
 /* Validando o facto de nao se introduzir um container num ship com a capacidade excedida */
 create or replace trigger test_number_containers_not_exceed_capacity
-before insert or update of idContainer on funcionario
-for each row
-declare 
-number_of_containers number(5);
-capacity number(5);
-
+    before insert or update on ContainerManifest for each row
+declare
+    number_of_containers number(5);
+    capacity number(11);
+    mmsi number(11);
+    ex_erro EXCEPTION;
+    id_Manifest ContainerManifest.idManifest%type;
+    cursor cursor_manifest is
+        SELECT distinct c.IDMANIFEST
+        FROM CONTAINERMANIFEST c WHERE
+                c.IDMANIFEST IN ( SELECT m.IDMANIFEST FROM MANIFEST m WHERE m.idManifest = :new.idManifest AND m.IDTRIP IN (SELECT  t.IDTRIP
+                                                                                                                            FROM TRIP t INNER JOIN VEHICLE v ON t.IDVEHICLE LIKE v.IDVEHICLE
+                                                                                                                            WHERE v.IDVEHICLE IN (SELECT IdVehicle FROM SHIP)));
+    cursor cursor_mmsi is SELECT mmsi FROM SHIP WHERE idVehicle IN (SELECT v.IdVehicle from Vehicle v where IDVEHICLE = (SELECT t.IDVEHICLE from Trip t where t.IdTrip = (SELECT m.idTrip FROM MANIFEST m WHERE m.IDMANIFEST = id_Manifest)));
 
 begin
-number_of_containers := func_getcontainersfromonemanifest(2);
-capacity := func_getcontainersfromonemanifest(987654321);
-if : capacity = number_of_containers then
-raise_application_error(-20000,'TENTOU INTRODUZIR MAIS CONTENTORES DO QUE A CAPACIDADE DO NAVIO');
-end if;
+
+    fetch cursor_manifest into id_Manifest;
+    fetch cursor_mmsi into mmsi;
+    number_of_containers := func_getcontainersfromonemanifest(id_Manifest);
+    capacity := func_getcapacityfromgivenship(mmsi);
+    if capacity <= number_of_containers then
+        raise_application_error(-20009,'TENTOU INTRODUZIR MAIS CONTENTORES DO QUE A CAPACIDADE DO NAVIO');
+    end if;
 end;
-/
