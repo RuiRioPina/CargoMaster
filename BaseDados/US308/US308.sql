2a90f84be001542dda955d843ca3ba33a62c8811
RuiRioPina <1201568@isep.ipp.pt>
2021-12-22 11:22:36 +0000

    <G76-168> #comment Updating the implementation of the US308 to solve the trigger not firing error. [Review]

--- BaseDados/US308/US308.sql
+++ BaseDados/US308/US308.sql
@@ -4,24 +4,22 @@ create or replace trigger test_number_containers_not_exceed_capacity
 declare
     number_of_containers number(5);
     capacity number(11);
-    mmsi number(11);
+    p_mmsi number(11);
     ex_erro EXCEPTION;
     id_Manifest ContainerManifest.idManifest%type;
-    cursor cursor_manifest is
-        SELECT distinct c.IDMANIFEST
+
+
+begin
+        SELECT distinct c.IDMANIFEST into id_Manifest 
         FROM CONTAINERMANIFEST c WHERE
                 c.IDMANIFEST IN ( SELECT m.IDMANIFEST FROM MANIFEST m WHERE m.idManifest = :new.idManifest AND m.IDTRIP IN (SELECT  t.IDTRIP
                                                                                                                             FROM TRIP t INNER JOIN VEHICLE v ON t.IDVEHICLE LIKE v.IDVEHICLE
                                                                                                                             WHERE v.IDVEHICLE IN (SELECT IdVehicle FROM SHIP)));
-    cursor cursor_mmsi is SELECT mmsi FROM SHIP WHERE idVehicle IN (SELECT v.IdVehicle from Vehicle v where IDVEHICLE = (SELECT t.IDVEHICLE from Trip t where t.IdTrip = (SELECT m.idTrip FROM MANIFEST m WHERE m.IDMANIFEST = id_Manifest)));
-
-begin
-
-    fetch cursor_manifest into id_Manifest;
-    fetch cursor_mmsi into mmsi;
-    number_of_containers := func_getcontainersfromonemanifest(id_Manifest);
-    capacity := func_getcapacityfromgivenship(mmsi);
+    SELECT mmsi into p_mmsi FROM SHIP WHERE idVehicle IN (SELECT v.IdVehicle from Vehicle v where IDVEHICLE = (SELECT t.IDVEHICLE from Trip t where t.IdTrip = (SELECT m.idTrip FROM MANIFEST m WHERE m.IDMANIFEST = :new.idManifest)));
+    number_of_containers := func_getContainersFromOneManifest(id_Manifest);
+    capacity := func_getcapacityfromgivenship(p_mmsi);
     if capacity <= number_of_containers then
         raise_application_error(-20009,'TENTOU INTRODUZIR MAIS CONTENTORES DO QUE A CAPACIDADE DO NAVIO');
     end if;
 end;
+
