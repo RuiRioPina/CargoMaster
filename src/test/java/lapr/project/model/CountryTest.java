a6f59a2291f204ae81020bcebe7fad281a7cb2a9
rafal <1201566@isep.ipp.pt>
2021-12-23 16:19:08 +0000

    <GG76-161>   #comment    [US301] Small correction

--- src/test/java/lapr/project/model/CountryTest.java
+++ src/test/java/lapr/project/model/CountryTest.java
@@ -26,6 +26,6 @@ class CountryTest {
         Country aa = new Country(new Continent("America"),new Location("17.25","-88.766667"),"Belize","BZ","BLZ",397.6,"Belmopan");
 
        String name = "Belize";
-        assertEquals(name,aa.getLocation().toString());
+        assertEquals(name,aa.getName());
     }
 }
