66aa3bc3e0721120a969ceb3b9d748def0231386
jorgefcferreira <1201564@isep.ipp.pt>
2021-12-24 11:26:31 +0000

    <G76-169> #time 3h #comment US309 PL/SQL solution update

--- BaseDados/US309/US309.sql
+++ BaseDados/US309/US309.sql
@@ -6,18 +6,18 @@ ex_erro EXCEPTION;
 bool boolean;
 start_date Date;
 end_date Date;
-id_vehicle trip.idvehicle%type;
+id_vehicle trip.startvehicle%type;
 total number;
-cursor cursors is select distinct idVehicle, startDateTrip, endDateTrip from Trip where idVehicle = :new.idVehicle;
+cursor cursors is select distinct startVehicle, startDateTrip, endDateTrip from Trip where startVehicle = :new.startVehicle;
 
 begin
 open cursors;
 bool := false;
 loop
 fetch cursors into id_vehicle, start_date, end_date;
-select count(idVehicle) into total
+select count(startVehicle) into total
 from trip 
-where idVehicle = id_vehicle
+where startVehicle = id_vehicle
 and (:new.startDateTrip between start_date and end_date) or (:new.endDateTrip between start_date and end_date);
 
 if total > 1 then
@@ -36,5 +36,3 @@ ex_erro then
 raise_application_error (-20009, 'The ship is not available so it is not possible to register a trip and consenquently a cargo manifest');
 
 end ;
-
-/
