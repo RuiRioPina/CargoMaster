580e748ae73388059ec73668ae802cfcfb5f8d7f
RuiRioPina <1201568@isep.ipp.pt>
2021-12-28 14:16:30 +0100

    <G76-163> #time 1h #comment Solving the no data found problem by counting the number of containers in the new manifest directly. [Review]

--- BaseDados/US308/US308.sql
+++ BaseDados/US308/US308.sql
@@ -10,13 +10,12 @@ declare
 
 
 begin
-        SELECT distinct c.IDMANIFEST into id_Manifest
+        SELECT COUNT(c.IDMANIFEST) into number_of_containers
 FROM CONTAINERMANIFEST c WHERE
         c.IDMANIFEST IN ( SELECT m.IDMANIFEST FROM MANIFEST m WHERE m.idManifest = :new.idManifest AND m.IDTRIP IN (SELECT  t.IDTRIP
                                                                                                                     FROM TRIP t INNER JOIN TRIPVEHICLE tv ON t.IDTRIP LIKE tv.IDTRIP
                                                                                                                     WHERE tv.IDVEHICLE IN (SELECT IdVehicle FROM Vehicle v WHERE v.IDVEHICLE IN (SELECT IdVehicle FROM SHIP))));
 SELECT mmsi into p_mmsi FROM SHIP WHERE idVehicle IN (SELECT v.IdVehicle from Vehicle v where v.IDVEHICLE = (SELECT tv.IDVEHICLE from TripVehicle tv WHERE tv.IDTRIP IN (SELECT t.IDTRIP from Trip t WHERE t.IdTrip = (SELECT m.idTrip FROM MANIFEST m WHERE m.IDMANIFEST = :new.idManifest))));   
-    number_of_containers := func_getContainersFromOneManifest(id_Manifest);
     capacity := func_getcapacityfromgivenship(p_mmsi);
     if capacity <= number_of_containers then
         raise_application_error(-20009,'TENTOU INTRODUZIR MAIS CONTENTORES DO QUE A CAPACIDADE DO NAVIO');
