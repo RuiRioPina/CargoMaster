f6a57eed86b2afd964c0c06f5ce1b3706013b770
RuiRioPina <1201568@isep.ipp.pt>
2021-12-29 15:36:58 +0100

    <G76-173> #time 1h #comment Changing the array from the fill_array.c to main.c. [Review]

--- ARQCP/fill_array.c
+++ ARQCP/fill_array.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-char *array[20][20][20] = {0};
 
-void fill_array() {
+void fill_array(char *array[20][20][20]) {
    static const char filename[] = "ContainersInManifest.csv";  
    FILE *file = fopen ( filename, "r" );  
    if ( file != NULL ) 
@@ -33,27 +32,6 @@ void fill_array() {
 				
 		}
 		
-		
-		
-		/**Prints for debugging US313 | To be deleted later on
-		for(int i = 0; i < 20;i++) {
-		for(int j = 0; j < 20;j++) {
-		for(int k = 0; k < 20;k++) {
-			printf("array[%d][%d][%d] = %s\n",i,j,k,array[i][j][k]);
-		}	
-		}	
-		}
-		printf("\n");
-
-		**/
-		/**Just to test in each members pc, it is importing the containers**/
-		printf("array[%d][%d][%d] = %s\n",1,1,1,array[1][1][1]);
-		printf("array[%d][%d][%d] = %s\n",3,3,1,array[3][3][1]);
-		printf("array[%d][%d][%d] = %s\n",4,4,4,array[4][4][4]);
-		printf("array[%d][%d][%d] = %s\n",7,5,1,array[7][5][1]);
-		printf("array[%d][%d][%d] = %s\n",8,5,8,array[8][5][8]);
-		printf("array[%d][%d][%d] = %s\n",12,12,12,array[12][12][12]);
-
 
 
 }
--- ARQCP/main.c
+++ ARQCP/main.c
@@ -1,7 +1,34 @@
+#include <stdio.h>
+
 #include "fill_array.h"
+char *array[20][20][20] = {0};
 int main ( void )
 {
-   fill_array();
+
+   fill_array(array);
+
+		
+		
+		/**Prints for debugging US313 | To be deleted later on
+		for(int i = 0; i < 20;i++) {
+		for(int j = 0; j < 20;j++) {
+		for(int k = 0; k < 20;k++) {
+			printf("array[%d][%d][%d] = %s\n",i,j,k,array[i][j][k]);
+		}	
+		}	
+		}
+		printf("\n");
+
+		**/
+		
+		printf("array[%d][%d][%d] = %s\n",1,1,1,array[1][1][1]);
+		printf("array[%d][%d][%d] = %s\n",3,3,1,array[3][3][1]);
+		printf("array[%d][%d][%d] = %s\n",4,4,4,array[4][4][4]);
+		printf("array[%d][%d][%d] = %s\n",7,5,1,array[7][5][1]);
+		printf("array[%d][%d][%d] = %s\n",8,5,8,array[8][5][8]);
+		printf("array[%d][%d][%d] = %s\n",12,12,12,array[12][12][12]);
+
+
    return 0;
 }
 
